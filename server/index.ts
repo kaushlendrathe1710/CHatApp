f95ff99b1f134a172c595569a488a8cbb3726679
giriexplores <aditkumar0715@gmail.com>
2025-11-07 12:01:56 +0530

    fixed enviroment issue configure dotenv

--- server/index.ts
+++ server/index.ts
@@ -2,6 +2,7 @@ import express, { type Request, Response, NextFunction } from "express";
 import { registerRoutes, broadcastToConversation } from "./routes";
 import { setupVite, serveStatic, log } from "./vite";
 import { storage } from "./storage";
+import "dotenv/config";
 
 const app = express();
 
